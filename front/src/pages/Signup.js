249ea35c00847a8d9e92ba64f0e10dc5cd9b6ca6
abj95630 <abah20000@gmail.com>
2023-03-16 15:31:04 +0100

    registration page created

--- front/src/pages/Signup.js
+++ front/src/pages/Signup.js
@@ -40,79 +40,115 @@ const Signup = () => {
 
   return (
     <>
-      <form className="flex flex-col items-center">
-        <input
+    <div className="bg-beige h-screen pt-10">
+      <form className="bg-white ml-[33%] inline-flex w-1/3 flex-col items-center pt-10 pb-6 rounded-sm">
+        <div className="font-bold text-4xl mb-3 w-[40%]">sign up</div>
+        <div className="w-[40%] text-marineblue font-bold text-xl">It's my first journey with VINMA</div>
+        <div className="w-[40%] my-1 mb-4">Sign up in order to post</div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>name *</label>
+          <input
           type="text"
-          placeholder="name"
-          className="border-[1px] mt-5"
+          placeholder="enter your name"
+          className="border-[1.5px] p-1 rounded-sm p-1.5"
           value={name}
           onChange={(event) => {
             setName(event.target.value);
           }}
-        />
-        <input
-          type="text"
-          placeholder="lastname"
-          className="border-[1px] mt-5"
-          value={lastname}
-          onChange={(event) => {
-            setLastname(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="nickname"
-          className="border-[1px] mt-5"
-          value={nickname}
-          onChange={(event) => {
-            setNickname(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="address"
-          className="border-[1px] mt-5"
-          value={address}
-          onChange={(event) => {
-            setAddress(event.target.value);
-          }}
-        />
-        <input
-          type="email"
-          placeholder="email"
-          className="border-[1px] mt-5"
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="border-[1px] mt-5"
-          value={password}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="confirm password"
-          className="border-[1px] mt-5"
-          value={confirmPassword}
-          onChange={(event) => {
-            setConfirmPassword(event.target.value);
-          }}
-        />
-        <input type="file" className="mt-5" />
+          required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>last name *</label>
+          <input
+            type="text"
+            placeholder="enter your last name"
+            className="border-[1.5px] p-1 rounded-sm p-1.5"
+            value={lastname}
+            onChange={(event) => {
+              setLastname(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>nickname *</label>
+          <input
+            type="text"
+            placeholder="enter your nickname"
+            className="border-[1.5px] p-1 rounded-sm p-1.5"
+            value={nickname}
+            onChange={(event) => {
+              setNickname(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>address *</label>
+          <input
+            type="text"
+            placeholder="enter your address"
+            className="border-[1.5px] p-1 rounded-sm p-1.5"
+            value={address}
+            onChange={(event) => {
+              setAddress(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>email *</label>
+          <input
+            type="email"
+            placeholder="enter your email"
+            className="border-[1.5px] p-1 rounded-sm p-1.5"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>password *</label>
+          <input
+            type="password"
+            placeholder="enter your password"
+            className="border-[1.5px] p-1 rounded-sm p-1.5"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 mb-4 w-[40%]">
+          <label>confirm password *</label>
+          <input
+            type="password"
+            placeholder="confirm your password"
+            className="border-[1.5px] p-1 rounded-sm p-1.5"
+            value={confirmPassword}
+            onChange={(event) => {
+              setConfirmPassword(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-y-1 w-[40%]">
+          <label>avatar</label>
+          <input type="file" className="mt-1 mb-2"/>
+        </div>
         <button
           type="submit"
-          className="border-[1px] bg-green-500 mt-5 px-5 py-2"
+          className="border-b-2 border-darkgold transition duration-200 hover:bg-darkgold text-white mt-5 px-5 py-2 bg-gold rounded-sm"
           onClick={handleSubmit}
         >
-          Sign Up
+          sign up
         </button>
       </form>
+      </div>
     </>
   );
 };
