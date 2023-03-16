cecba50b2c5a526545725bddc5bbf39c067802d7
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-03-16 19:04:32 +0100

    add save cart localstorage + counter of number of products in cart

--- front/src/components/Cart.jsx
+++ front/src/components/Cart.jsx
@@ -1,10 +1,10 @@
 // import packages
 import { ChevronLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
 
-// import assets
-import Meuble from "../img/meuble01.jpg";
-
 const Cart = ({ toggleCart, cart, removeFromCart }) => {
+
+  let total = 0;
+
   return (
     <>
       <div className="cart bg-white  mx-4 mt-4  p-5">
@@ -17,21 +17,28 @@ const Cart = ({ toggleCart, cart, removeFromCart }) => {
         </div>
       </div>
       {cart.map((product) => {
+        total += product.price;
         return (
           <div className="flex flex-col cart bg-white  mx-4 mt-[1px] p-5">
             <div className="flex justify-end">
               <TrashIcon className="h-6 w-6" onClick={removeFromCart} />
+         
             </div>
             <div className="flex items-end justify-between mt-5">
-              <img
-                src={product.picture}
-                alt=""
-                className="w-24 h-24 object-cover"
-              />
-              <div className="text-product ">
-                <p className="font-bold">{product.title}</p>
-                <p className="text-sm w-[230px] mt-2">{product.description}</p>
+              <div className="flex items-end">
+                <img
+                  src={product.picture.Meuble01}
+                  alt=""
+                  className="w-24 h-24 object-cover"
+                />
+                <div className="text-product ml-5">
+                  <p className="font-bold">{product.title}</p>
+                  <p className="text-sm w-[230px] mt-2">
+                    {product.description}
+                  </p>
+                </div>
               </div>
+
               <div className="price text-xl font-bold">{product.price}</div>
             </div>
           </div>
@@ -41,7 +48,7 @@ const Cart = ({ toggleCart, cart, removeFromCart }) => {
       <div className="flex flex-col cart bg-white  mx-4 mt-[1px] p-5">
         <div className="flex items-end justify-between">
           <p className="font-bold">Total</p>
-          <div className="price text-3xl font-bold">9.38 €</div>
+          <div className="price text-3xl font-bold">{total.toFixed(2)} €</div>
         </div>
         <button className="bg-orange-400 text-white p-2 mt-7 rounded">
           Commander
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,196 +1,99 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { useState, useEffect } from "react";
+
+//import components
+import Header from "../components/Header";
+
+//import assets
+import Meuble01 from "../img/meuble01.jpg";
 
 const Home = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 6
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 6
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 4
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 3
-        }
-      };
+  const [cart, setCart] = useState([]);
 
-      const responsive2 = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5,
-          slideToSlide: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 5,
-          slideToSlide: 4
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 3,
-          slideToSlide: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slideToSlide: 1
-        }
-      };
-    return (
-        <>
-        <div className="Home w-[100vw] h-[125vh]  mt-10 mr-10 ml-10"> 
+  const addToCart = (product) => {
+    const newCart = [...cart];
+    const productExist = newCart.find((elem) => elem.id === product.id);
+ 
+    if (productExist) {
+      console.log("le produit est deja dans la panier");
+    } else {
+      newCart.push(product);
+    }
+    setCart(newCart);
+    saveCart(newCart)
+  };
 
-        {/* Carousel pour category menu */}
+  const removeFromCart = (product) => {
+    const newCart = [...cart];
+    const productInTab = newCart.find((elem) => elem.id === product.id);
+    const index = newCart.indexOf(productInTab);
+    newCart.splice(index, 1);
+    setCart(newCart);
+    saveCart(newCart)
+  };
 
-          <Carousel responsive={responsive}>
-              <div className="card">
-                <img className="product--image rounded-full border border-gray-100 shadow-lg w-24 h-24"
-                    src="https://static.galerieslafayette.com/media/3004086/300408683792/G_300408683792_1212_VFP_1.jpg"
-                    alt="product image"/>
-                    <span className="ml-8"> Bed </span>     
-              </div>
-              <div className="card">
-                <img className="product--image rounded-full border border-gray-100 shadow-lg w-24 h-24"
-                    src="https://cdn.laredoute.com/products/0/0/9/0093e7dd5f1b25cd1fb79dde54a7ca4f.jpg?width=900&dpr=1"
-                    alt="product image"/>
-                    <span className="ml-5">Cabinet</span>
-              </div>
-              <div className="card">
-                  <img className="product--image rounded-full border border-gray-100 shadow-lg w-24 h-24"
-                      src="https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_2000/img/canape-clic-clac-3-places-en-velours-vert-1000-16-15-198168_1.jpg"
-                      alt="product image"/>
-                      <span className="ml-7">Sofa</span>
-              </div>
-              <div className="card">
-                  <img className="product--image rounded-full border border-gray-100 shadow-lg w-24 h-24"
-                      src="https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_2000/img/chaise-vintage-en-velours-vieux-rose-et-metal-imitation-chene-1000-4-30-210073_1.jpg"
-                      alt="product image"/>
-                      <span className="ml-7">Chair</span>
-              </div>
-              <div className="card">
-                  <img className="product--image rounded-full border border-gray-100 shadow-lg w-24 h-24"
-                      src="https://cdn.laredoute.com/products/1/8/5/185539d0b362ea30b16462b30d8f1e20.jpg?width=700&dpr=1"
-                      alt="product image"/>
-                      <span className="ml-7" >Table</span>
-              </div>
-              <div className="card">
-                  <img className="product--image rounded-full border border-gray-100 shadow-lg w-24 h-24"
-                      src="https://cdn.laredoute.com/products/c/4/0/c400ea36ee52792fc307da3f3eaee21c.jpg?width=800&dpr=1"
-                      alt="product image"/>
-                      <span className="ml-6">Others</span>
-              </div>
-          </Carousel>
+  const saveCart = (cart) => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
+  
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
 
-              <h2 className= "font-medium text-4xl text-stone-800 mt-10 mr-10 ml-10">you may also like</h2>
+  const products = [
+    {
+      id: "sfkgjhfdklhdkljhgdkf",
+      title: "Meuble TV en teck",
+      description:
+        "Meuble tv, portes coulissante, niche et rangement vinyles bleu canard et teck.",
+      picture: { Meuble01 },
+      price: 9.38,
+    },
+    {
+      id: "fghpoknighpokghpk",
+      title: "Meuble TV en teck",
+      description:
+        "Meuble tv, portes coulissante, niche et rangement vinyles bleu canard et teck.",
+      picture: { Meuble01 },
+      price: 9.38,
+    },
+    {
+      id: "cnvbfxjvhbyttyytytyty",
+      title: "Meuble TV en teck",
+      description:
+        "Meuble tv, portes coulissante, niche et rangement vinyles bleu canard et teck.",
+      picture: { Meuble01 },
+      price: 9.38,
+    },
+  ];
 
-          {/* adding products */}
-          <section className=" mt-6 mr-10">
-            <Carousel responsive={responsive2}>
-              <div className="card mb-8 mt-8 ml-8 bg-white">
-                    <img className="w-[200px] h-[200px] object-cover pl-6" 
-                        src="https://cdn1.bobochicparis.com/191749-thickbox_default/canape-droit-fixe-3-places-cloud-avec-pouf.jpg"
-                        alt="img"/>
-                        <div className="pl-4 pr-4 pb-4">
-                          <h2 className="font-bold">Title product</h2>
-                          <h2 className= "text-4xl pt-2 pb-2">22 €</h2>
-                          <button
-                            type="submit"
-                            className="py-2 px-14 bg-amber-400 hover:bg-yellow-600 rounded-md text-white text-sm"
-                           >
-                            Add to cart
-                          </button>
-                        </div>
+  return (
+    <>
+      <div className="bg-beige h-screen">
+        <Header cart={cart} setCart={setCart} removeFromCart={removeFromCart} />
+        <div className="product flex flex-row m-5">
+          {products.map((product, index) => {
+            return (
+              <div
+                className="bg-white p-5 m-2"
+                onClick={() => {
+                  addToCart(product);
+                }}
+                key={product.id}
+              >
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+                <img className="h-20" src={product.picture.Meuble01} alt="" />,
+                <p>{product.price}</p>
               </div>
-              <div className="card mb-8 mt-8 ml-8 bg-white">
-                    <img className="w-[200px] h-[200px] object-cover pl-6" 
-                        src="https://static.tikamoon.com/product_photos/3965_A_HD_001/1200-1200-armoire-de-rangement-en-pin-et-cannage-ninon.webp?t=1665709167"
-                        alt="img"/>
-                        <div className="pl-4 pr-4 pb-4">
-                          <h2 className="font-bold">Title product</h2>
-                          <h2 className= "text-4xl pt-2 pb-2">22 €</h2>
-                          <button
-                            type="submit"
-                            className="py-2 px-14 bg-amber-400 hover:bg-yellow-600 rounded-md text-white text-sm"
-                           >
-                            Add to cart
-                          </button>
-                        </div>
-              </div>
-              <div className="card mb-8 mt-8 ml-8 bg-white">
-                    <img className="w-[200px] h-[200px] object-cover pl-6" 
-                        src="https://static.tikamoon.com/product_photos/1625__1200_000/1200-1200-chaise-en-teck-massif-jonak.webp?t=1665573674"
-                        alt="img"/>
-                        <div className="pl-4 pr-4 pb-4">
-                          <h2 className="font-bold">Title product</h2>
-                          <h2 className= "text-4xl pt-2 pb-2">22 €</h2>
-                          <button
-                            type="submit"
-                            className="py-2 px-14 bg-amber-400 hover:bg-yellow-600 rounded-md text-white text-sm"
-                           >
-                            Add to cart
-                          </button>
-                        </div>
-              </div>
-              <div className="card mb-8 mt-8 ml-8 bg-white">
-                    <img className="w-[200px] h-[200px] object-cover pl-6" 
-                        src="https://static.tikamoon.com/product_photos/835__HD_002/1200-1200-table-en-teck-massif-coffee-tek-6-8-pers.webp?t=1665819563"
-                        alt="img"/>
-                        <div className="pl-4 pr-4 pb-4">
-                          <h2 className="font-bold">Title product</h2>
-                          <h2 className= "text-4xl pt-2 pb-2">22 €</h2>
-                          <button
-                            type="submit"
-                            className="py-2 px-14 bg-amber-400 hover:bg-yellow-600 rounded-md text-white text-sm"
-                           >
-                            Add to cart
-                          </button>
-                        </div>
-              </div>
-              <div className="card mb-8 mt-8 ml-8 bg-white">
-                    <img className="w-[200px] h-[200px] object-cover pl-6" 
-                        src="https://cdn1.bobochicparis.com/197455-thickbox_default/lit-180x200-cm-oceane-avec-lattes-massives-tissu-texture.jpg"
-                        alt="img"/>
-                        <div className="pl-4 pr-4 pb-4">
-                          <h2 className="font-bold">Title product</h2>
-                          <h2 className= "text-4xl pt-2 pb-2">22 €</h2>
-                          <button
-                            type="submit"
-                            className="py-2 px-14 bg-amber-400 hover:bg-yellow-600 rounded-md text-white text-sm"
-                           >
-                            Add to cart
-                          </button>
-                        </div>
-              </div>
-              </Carousel>
-
-        {/* footer section */}
-        </section>
-        <section className="flex flex-col justify-center items-center m-8 space-x-4 font-normal">
-          <div className="p-4">
-          <img className="ml-[100px]"
-          src="https://assets.made-in-meubles.com/static/version1678969329/frontend/Mim/wex/fr_FR/images/footer/phone.svg"/> Secure and confidential payment</div>
-          <div className="p-5">
-          <img className="ml-[60px]"
-          src="https://assets.made-in-meubles.com/static/version1678969329/frontend/Mim/wex/fr_FR/images/footer/lock.svg"/>
-            Delivery in France</div>
-          <div className="p-3">
-          <img className="ml-[80px]"
-          src="https://assets.made-in-meubles.com/static/version1678969329/frontend/Mim/wex/fr_FR/images/footer/truck.svg"/>
-            Reactive technical support</div>
-        </section>
-        </div>  
-      
-        </>
-    )
-}
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
