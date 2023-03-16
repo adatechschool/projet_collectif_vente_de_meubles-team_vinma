9871682b101a6058ec9fc8a9d45dacf234e6dd82
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-03-16 16:38:43 +0100

    first commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,12 +4,14 @@ import './App.css';
 //import Components / Pages
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 
 function App() {
   return (<>
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Home/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
